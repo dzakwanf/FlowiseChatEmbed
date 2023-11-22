f257b13576b003938aa1f02d5c123b977317e874
dzakwan.falih <dzakwan.falih@fisclouds.com>
2023-11-22 17:59:40 +0700

    update code

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,7 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> Fisclouds X Google X Bank BJB</span>
       </a>
     </span>
   )
