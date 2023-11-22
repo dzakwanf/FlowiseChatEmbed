3addc7aa3930b2d0db148a9893ee33feea57cba7
dzakwan.falih <dzakwan.falih@fisclouds.com>
2023-11-22 18:02:00 +0700

    update code

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
     }}>Powered by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'#'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
